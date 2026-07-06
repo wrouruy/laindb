cf6495ff0c1be147a08c12ae0936fafa68bcf59e
wrouruy <wroyruy@gmail.com>
2026-07-06 15:31:29 +0300

    added RESP parsing

--- main.cpp
+++ main.cpp
@@ -1,53 +1,86 @@
-#include <iostream>
-#include <vector>
 #define ASIO_STANDALONE
 #include <asio.hpp>
+#include <iostream>
+#include <memory>
+#include "parse_resp.hpp"
 
 using asio::ip::tcp;
 
-int main() {
-    try {
-        asio::io_context io_context;
-
-        // create listener on port 8080 for ipv4
-        tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 8080));
-        std::cout << "Server is listening on port 8080..." << std::endl;
-
-        // server just returns the same client's own data, echo yet
-        while(1) {
-            
-            // create socket for further client
-            tcp::socket socket(io_context);
-
-            // wait for connection
-            acceptor.accept(socket);
-            std::cout << "Client connected successfully!" << std::endl;
-
-            // buffer for reading client data
-            std::vector<char> buffer(1024);
-            asio::error_code error;
-
-            // read client data
-            size_t length = socket.read_some(asio::buffer(buffer), error);
-
-            if (!error) {
-                std::cout << "Received from client: ";
-                std::cout.write(buffer.data(), length) << std::endl;
-
-                // send this data back, echo
-                asio::write(socket, asio::buffer(buffer, length));
-            } else if (error != asio::error::eof) {
-                throw asio::system_error(error);
-            }
-
-        }
-
-        std::cout << "Server shutting down." << std::endl;
-
-    } catch (std::exception& e) {
-        std::cerr << "Exception: " << e.what() << std::endl;
-        return 1;
+class Session : public std::enable_shared_from_this<Session>
+{
+public:
+    Session(tcp::socket socket)
+        : socket_(std::move(socket))
+    {
     }
 
-    return 0;
+    void start()
+    {
+        std::cout << "Client connected!\n";
+        read();
+    }
+
+private:
+    void read()
+    {
+        auto self = shared_from_this();
+
+        socket_.async_read_some(
+            asio::buffer(buffer_),
+            [this, self](std::error_code ec, std::size_t length)
+            {
+                if (ec)
+                {
+                    std::cout << "Client disconnected\n";
+                    return;
+                }
+                
+                const char* buf = buffer_.data();
+
+                std::vector<RespToken> resp = parse_resp(buf, length);
+
+                const char* enum2str[] = { "SIMPLE_STR", "SIMPLE_ERR", "INTEGER", "BULK_STR" };
+
+                for (int i = 0; i < resp.size(); i++) {
+                    std::cout << "type: " << enum2str[static_cast<int>(resp[i].type)] << ", value: " << resp[i].value << std::endl;
+                }                
+                
+                asio::async_write(
+                    socket_,
+                    asio::buffer("+OK\r\n"),
+                    [this, self](std::error_code ec, std::size_t)
+                    {
+                        if (!ec)
+                            read();
+                    });
+            });
+    }
+
+    tcp::socket socket_;
+    std::array<char, 1024> buffer_;
+};
+
+int main()
+{
+    asio::io_context io;
+
+    tcp::acceptor acceptor(io, tcp::endpoint(tcp::v4(), 8080));
+
+    std::function<void()> accept;
+
+    accept = [&]()
+    {
+        acceptor.async_accept(
+            [&](std::error_code ec, tcp::socket socket)
+            {
+                if (!ec)
+                    std::make_shared<Session>(std::move(socket))->start();
+
+                accept();
+            });
+    };
+
+    accept();
+
+    io.run();
 }
---
+++ parse_resp.hpp
@@ -0,0 +1,73 @@
+#pragma once
+
+enum class RespType {
+    SIMPLE_STR,     // +
+    SIMPLE_ERR,     // -
+    INTEGERS,       // :
+    BULK_STR,       // $
+    NULL_BULK_STR,  // $-1
+    ARRAYS,         // *
+    NULLS,          // _
+    BOOLEANS,       // #
+    DOUBLES,        // ,
+    BIG_NUMBERS,    // (
+    BULK_ERR,       // !
+    VERBATIM_STR,   // =
+    MAPS,           // %
+    ATTRIBUTES,     // |
+    SETS,           // ~
+    PUSHES          // >
+};
+
+struct RespToken {
+    std::string value;
+    RespType type;
+};
+
+std::vector<RespToken> parse_resp(std::string buf, std::size_t length) {
+    std::vector<RespToken> res;
+    for (std::size_t i = 0; i < length; i++) {
+        std::size_t lineEnd = buf.find("\r\n", i);
+        switch (buf[i]) {
+            case '+':
+                res.push_back({
+                    buf.substr(i + 1, lineEnd),
+                    RespType::SIMPLE_STR
+                });
+                i += lineEnd;
+                break;
+
+            case '-':
+                res.push_back({
+                    buf.substr(i + 1, lineEnd),
+                    RespType::SIMPLE_ERR
+                });
+                i += lineEnd;
+                break;
+
+            case ':':
+                res.push_back({
+                    buf.substr(i + 1, lineEnd),
+                    RespType::INTEGERS
+                });
+                i += lineEnd;
+                break;
+
+            case '$':
+                std::string length_str = buf.substr(i + 1, lineEnd - (i + 1));
+                int length = std::stoi(length_str);
+
+                size_t content_start = lineEnd + 2;
+                std::string content = buf.substr(content_start, length);
+
+                res.push_back({
+                    content,
+                    RespType::BULK_STR
+                });
+                break;
+        }
+    }
+
+    return res;
+}
+
